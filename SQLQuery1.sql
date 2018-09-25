4829b0fb869c18f13e6f1aaf4af63eaf9beee2a2
Adans Victor <adans8364@gmail.com>
2018-09-25 14:46:26 -0300

    a

---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿select * from Produtos
+
+
+Select  * from information_schema.columns WHERE TABLE_NAME='Produtos' 
+
+select *  from Pedidos
+
+ALTER TABLE Pedidos
+  ADD IdProd smallint 
+  references Produtos(IdProd);
+
+drop table Pedidos;
+
+select * from pedido
+
+select * from item_pedido
